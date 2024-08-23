cb161137f18820e4b26001e7da5d9470180bb46a
jhyuck-solver <jerrykbayes0814@gmail.com>
2024-08-23 15:41:19 +0900

    일단은 된다.

--- streamlit/DashBoard.py
+++ streamlit/DashBoard.py
@@ -29,163 +29,162 @@ st.divider()
 st.subheader('년도 별 차량/전기차 등록대수 현황')
 
 # 데이터 로드
-# secrets.toml에서 정보 가져오기
-username = st.secrets["username"]
-password = st.secrets["password"]
-host = st.secrets["host"]
-port = st.secrets["port"]
-database_name = st.secrets["database"]
+conn = st.connection('postgresql', type='sql')
 
-# 데이터베이스 URL 구성
-DATABASE_URL = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database_name}"
+query = f"""
+SELECT 
+    public.vehicle_data.year AS year, 
+    public.vehicle_data.value AS registered,
+    public.vehicle_data.region AS region
+FROM 
+    public.vehicle_data
+"""
 
-# SQLAlchemy 엔진 생성
-engine = create_engine(DATABASE_URL)
+df_car = conn.query(query, ttl=600)
 
-# 데이터 불러오기 함수
-def load_data(table_name):
-    with engine.connect() as connection:
-        result = pd.read_sql(f"SELECT * FROM {table_name}", con=connection)
-    return result
+df_car_pivot = df_car.pivot(index='year', columns='region', values='registered').reset_index()
 
-df = load_data('vehicle_data')
-print(df)
+df_car_pivot['year'] = df_car_pivot['year'].astype('int')
 
-# # ECharts 옵션 설정
-# options = {
-#     "tooltip": {
-#         "trigger": 'axis',
-#         "axisPointer": {
-#             "type": 'cross'
-#         }
-#     },
-#     "legend": {
-#         "data": ['Total Vehicles', 'Electric Vehicles']
-#     },
-#     "xAxis": {
-#         "type": 'category',
-#         "boundaryGap": False,
-#         "data": years
-#     },
-#     "yAxis": {
-#         "type": 'value'
-#     },
-#     "series": [
-#         {
-#             "name": 'Total Vehicles',
-#             "type": 'line',
-#             "data": total_vehicle_registrations,
-#             "smooth": True,
-#         },
-#         {
-#             "name": 'Electric Vehicles',
-#             "type": 'line',
-#             "data": ev_registrations,
-#             "smooth": True,
-#         }
-#     ]
-# }
+df_car_pivot['total'] = df_car_pivot.iloc[:,1:].sum(axis=1)/10000
 
-# # Streamlit 애플리케이션에서 그래프 표시
-# st.title("Vehicle Registration Trends (2015 - 2023)")
-# st.write("Total vehicle registrations and electric vehicle registrations from 2015 to 2023.")
+query = f"""
+SELECT 
+    "연월" AS year, 
+    public.electric_vehicles."Value" AS registered,
+    public.electric_vehicles."Region" AS region
+FROM 
+    public.electric_vehicles
+"""
 
-# # ECharts 그래프 렌더링
-# st_echarts(options=options)
+df_ecar = conn.query(query, ttl=600)
 
-# st.divider()
+df_ecar_pivot = df_ecar.pivot(index='year', columns='region', values='registered').reset_index()
 
-# # 데이터 로드 (예시 데이터 경로 및 읽기)
-# geo_path = 'SIDO_MAP.json'
-# with open(geo_path, encoding='utf-8') as f:
-#     geo_data = json.load(f)
-
-# year_list = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
+df_ecar_pivot['total'] = df_ecar_pivot.iloc[:,1:].sum(axis=1)/10000
 
 
-# # 데이터 필터링
-# # 데이터 구성
-# regions = ['서울특별시', '경기도', '인천광역시', '부산광역시', '대구광역시', '대전광역시', '광주광역시', '울산광역시', '강원도', '충청북도', '충청남도', '전라북도', '전라남도', '경상북도', '경상남도', '제주특별자치도']
-# years = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
+# ECharts 옵션 설정
+options = {
+    "tooltip": {
+        "trigger": 'axis',
+        "axisPointer": {
+            "type": 'cross'
+        }
+    },
+    "legend": {
+        "data": ['Total Vehicles', 'Electric Vehicles']
+    },
+    "xAxis": {
+        "type": 'category',
+        "boundaryGap": False,
+        "data": df_car_pivot['year'].to_list()
+    },
+    "yAxis": {
+        "type": 'value'
+    },
+    "series": [
+        {
+            "name": 'Total Vehicles',
+            "type": 'line',
+            "data": df_car_pivot['total'].to_list(),
+            "smooth": True,
+        },
+        {
+            "name": 'Electric Vehicles',
+            "type": 'line',
+            "data": df_ecar_pivot['total'].to_list(),
+            "smooth": True,
+        }
+    ]
+}
 
-# # 연도별로 데이터를 반복하여 각 지역에 다른 값 할당
-# data = []
-# for year in years:
-#     values = list(range(10000, 26000, 1000))  # 각 지역별로 다르게 증가하는 값 예제
-#     for region, value in zip(regions, values):
-#         data.append({'year': year, 'region': region, 'value': value})
+# Streamlit 애플리케이션에서 그래프 표시
+st.title("Vehicle Registration Trends (2015 - 2023)")
+st.write("Total vehicle registrations and electric vehicle registrations from 2015 to 2023.")
 
-# # 데이터프레임 생성
-# df = pd.DataFrame(data)
+# ECharts 그래프 렌더링
+st_echarts(options=options)
 
-# # Sidebar 설정
-# with st.sidebar:
-#     st.title('🚗 Korea Dashboard')
+st.divider()
+
+# 데이터 로드 (예시 데이터 경로 및 읽기)
+geo_path = 'SIDO_MAP.json'
+with open(geo_path, encoding='utf-8') as f:
+    geo_data = json.load(f)
+
+# 데이터 필터링
+# 데이터 구성
+# df_cars = pd.merge(df_car, df_ecar, how='left', on='year', suffixes=('_car', '_elec_car'))
+
+# Sidebar 설정
+with st.sidebar:
+    st.title('🚗 Korea Dashboard')
     
-#     year_list = list(df['year'].unique())[::-1]
-#     selected_year = st.selectbox('Select a year', year_list, index=len(year_list)-1)
-#     df_selected_year = df[df['year'] == selected_year]
-#     df_selected_year_sorted = df_selected_year.sort_values(by="value", ascending=False)[:5]
+    year_list = list(df_ecar['year'].unique())[::-1]
+    selected_year = st.selectbox('Select a year', year_list, index=len(year_list)-1)
+    df_selected_year = df_ecar[df_ecar['year'] == selected_year]
+    df_selected_year_sorted = df_selected_year.sort_values(by="registered", ascending=False)[:5]
 
-# # Streamlit에서 2개의 컬럼 생성
-# col = st.columns((0.2, 0.8), gap='medium')
+# Streamlit에서 2개의 컬럼 생성
+col = st.columns((0.2, 0.8), gap='medium')
 
-# # 지도 그리기
-# with col[0]:
-#     st.markdown('#### Top States')
+# 지도 그리기
+with col[0]:
+    st.markdown('#### Top States')
 
-#     st.dataframe(df_selected_year_sorted,
-#                  column_order=("region", "value"),
-#                  hide_index=True,
-#                  width=None)
+    st.dataframe(df_selected_year_sorted,
+                 column_order=("region", "value"),
+                 hide_index=True,
+                 width=None)
 
-# with col[1]:
-#     st.subheader('시도 별 자동차 등록대수')
-#     latitude = 36.2
-#     longitude = 127.5
+with col[1]:
+    st.subheader('시도 별 자동차 등록대수')
+    latitude = 36.2
+    longitude = 127.5
 
-#     # 지도 생성
-#     m = folium.Map(location=[latitude, longitude],
-#                    zoom_start=6,
-#                    zoom_control=False,
-#                    tiles=None, 
-#                    dragging=False,
-#                    scrollWheelZoom=False,
-#                    doubleClickZoom=False)
+    # 지도 생성
+    m = folium.Map(location=[latitude, longitude],
+                   zoom_start=6,
+                   zoom_control=False,
+                   tiles=None, 
+                   dragging=False,
+                   scrollWheelZoom=False,
+                   doubleClickZoom=False)
 
-#     folium.TileLayer('cartodbpositron').add_to(m)
+    folium.TileLayer('cartodbpositron').add_to(m)
 
-#     # 색상 스케일 설정
-#     color_scale = folium.LinearColormap(colors=['#FFEDA0', '#FEB24C', '#F03B20'], 
-#                                         vmin=df['value'].min(), 
-#                                         vmax=df['value'].max())
+    # 색상 스케일 설정
+    color_scale = folium.LinearColormap(colors=['#FFEDA0', '#FEB24C', '#F03B20'], 
+                                        vmin=df_ecar['registered'].min(), 
+                                        vmax=df_ecar['registered'].max())
 
-#     # GeoJSON 데이터와 데이터프레임 결합하여 지도에 데이터 추가
-#     for feature in geo_data['features']:
-#         region_name = feature['properties']['CTP_KOR_NM']
-#         region_data = df_selected_year[df_selected_year['region'] == region_name]
+    # GeoJSON 데이터와 데이터프레임 결합하여 지도에 데이터 추가
+    for feature in geo_data['features']:
+        region_name = feature['properties']['CTP_KOR_NM']
+        region_data = df_selected_year[df_selected_year['region'] == region_name]
         
-#         if not region_data.empty:
-#             value = region_data['value'].iloc[0]
-#             color = color_scale(value)
+        if not region_data.empty:
+            value = region_data['registered'].iloc[0]
+            color = color_scale(value)
             
-#             folium.GeoJson(
-#                 feature,
-#                 style_function=lambda x, color=color: {
-#                     'fillColor': color,
-#                     'color': 'black',
-#                     'weight': 1,
-#                     'fillOpacity': 0.7,
-#                 },
-#                 tooltip=folium.Tooltip(f"{region_name}: {value}"),
-#                 highlight_function=lambda x: {'color': 'white', 'weight': 3, 'fillOpacity': 0.9}
-#             ).add_to(m)
+            folium.GeoJson(
+                feature,
+                style_function=lambda x, color=color: {
+                    'fillColor': color,
+                    'color': 'black',
+                    'weight': 1,
+                    'fillOpacity': 0.7,
+                },
+                tooltip=folium.Tooltip(f"{region_name}: {value}"),
+                highlight_function=lambda x: {'color': 'white', 'weight': 3, 'fillOpacity': 0.9}
+            ).add_to(m)
     
-#     color_scale.add_to(m)
-#     st.components.v1.html(m._repr_html_(), width=570, height=900)
+    color_scale.add_to(m)
+    st.components.v1.html(m._repr_html_(), width=570, height=900)
 
 
-# st.divider()
+st.divider()
 
 
 # with st.sidebar:
