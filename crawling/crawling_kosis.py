a16fa6839e95e0cf78043ddff4811c7bd8156940
hwarange <asdf78964@naver.com>
2024-08-23 12:19:10 +0900

    끝

--- crawling/crawling_kosis.py
+++ crawling/crawling_kosis.py
@@ -28,23 +28,8 @@ for i in range(len(results)):
         PRD_DE = PRD_DE[:4]
         data.append([PRD_DE, count, region])
 
-df = pd.DataFrame(data, columns=['연도', '값', '지역'])
+df = pd.DataFrame(data, columns=['year', 'value', 'region'])
 
-regions = list(OrderedDict.fromkeys(df['지역']))
-
-pivot_df = df.pivot(index='연도', columns='지역', values='값')
-
-pivot_df = pivot_df[regions]
-
-# 멀티인덱스 해제: 피벗된 데이터에서 칼럼 레이블 '지역'을 제거
-pivot_df.columns.name = None
-
-pivot_df = pivot_df.reset_index()
-
-# 결과 확인
-print(pivot_df)
-
-        
 from sqlalchemy import create_engine
 from sqlalchemy.types import Integer, String
 
@@ -52,33 +37,20 @@ db_url = 'postgresql+psycopg2://postgres:1234@localhost:5432/electr_vehicle'
 
 engine = create_engine(db_url, echo=True)
 
-pivot_df.to_sql(
+df.to_sql(
   'vehicle_data',
   engine,
   if_exists='replace', # replace: 덮어쓰기
   index=False,
   chunksize=5000,
   dtype={              # 데이터 열의 데이터 유형
-        '연도': String,
-        '서울': Integer,
-        '부산': Integer,
-        '대구': Integer,
-        '인천': Integer,
-        '광주': Integer,
-        '대전': Integer,
-        '울산': Integer,
-        '세종': Integer,
-        '경기': Integer,
-        '강원': Integer,
-        '충북': Integer,
-        '충남': Integer,
-        '전북': Integer,
-        '전남': Integer,
-        '경북': Integer,
-        '경남': Integer,
-        '제주': Integer,
+        'year': String,
+        'value': Integer,
+        'region': String,
+        
     }
 )
+
     
     #with psycopg2.connect(
     #host='localhost',
