32c838a5f32cb1403ff5244881b014add956dc6f
hwarange <asdf78964@naver.com>
2024-08-27 10:52:15 +0900

    보안상의 이유로 api key 없앰

--- crawling/crawling_kosis.py
+++ crawling/crawling_kosis.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 import psycopg2
 
 def fetch_kosis_data():
-    API_KEY = 'YzFmMDk4ZDBhZjU0YjRlMWU2ZGQ2YjFjOTc5YWY4ZDc='
+    API_KEY = ''
 
     url = f'https://kosis.kr/openapi/Param/statisticsParameterData.do?method=getList&apiKey={API_KEY}&itmId=13103873443T4+&objL1=ALL&objL2=13102873443B.0002+&objL3=13102873443C.0005+&objL4=&objL5=&objL6=&objL7=&objL8=&format=json&jsonVD=Y&prdSe=M&startPrdDe=201512&endPrdDe=202407&outputFields=TBL_NM+OBJ_NM+NM+ITM_NM+UNIT_NM+PRD_DE+&orgId=116&tblId=DT_MLTM_5498'
 
